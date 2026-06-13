05a9dbf5c03794522c5242907896e784061f85af
HoangDuc1205 <nguyenducgl1205@gmail.com>
2026-06-14 02:34:53 +0700

    Update java and be

--- app/src/main/java/com/teatrack_mcd_253eie502802_group02/shared/ui/NavBarClient.java
+++ app/src/main/java/com/teatrack_mcd_253eie502802_group02/shared/ui/NavBarClient.java
@@ -10,7 +10,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.teatrack_mcd_253eie502802_group02.R;
+import com.teatrack_mcd_253eie502802_group02.R;
 import com.teatrack_mcd_253eie502802_group02.client.Homepage;
 import com.teatrack_mcd_253eie502802_group02.client.Menu;
 import com.teatrack_mcd_253eie502802_group02.client.OrderHistory;
