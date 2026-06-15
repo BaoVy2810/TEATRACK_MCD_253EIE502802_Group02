b67ed9ca96b22eca3c50b91477aae8d69b6eabff
HoangDuc1205 <nguyenducgl1205@gmail.com>
2026-06-15 13:16:08 +0700

    fixed main activity huhu

--- app/src/main/java/com/teatrack_mcd_253eie502802_group02/MainActivity.java
+++ app/src/main/java/com/teatrack_mcd_253eie502802_group02/MainActivity.java
@@ -1,6 +1,5 @@
 package com.teatrack_mcd_253eie502802_group02;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -10,15 +9,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.teatrack_mcd_253eie502802_group02.R;
-import com.teatrack_mcd_253eie502802_group02.admin.AdminDashboard;
-import com.teatrack_mcd_253eie502802_group02.client.Homepage;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, AdminDashboard.class));
-        finish();
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
